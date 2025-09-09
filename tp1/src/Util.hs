e778c694339bc30017485f4ea805dd7c3b92bfed
hrancati <hrancati@renatre.org.ar>
2025-09-09 15:30:58 -0300

    ejercicio 2 completado

--- tp1/src/Util.hs
+++ tp1/src/Util.hs
@@ -11,12 +11,19 @@ alinearDerecha n s = espacios ++ s
         f x res = " " ++ res    -- lambda para el fold, simplemente concatena los espacios
         espacios = foldr f "" t -- generamos los espacios concatenando la lista por compresion
 
+
+recr :: b -> (a -> [a] -> b -> b) -> [a] -> b
+recr z _ []     = z
+recr z f (x:xs) = f x xs (recr z f xs)
+
 -- | Dado un índice y una función, actualiza el elemento en la posición del índice
 -- aplicando la función al valor actual. Si el índice está fuera de los límites
 -- de la lista, devuelve la lista sin cambios.
 -- El primer elemento de la lista es el índice 0.
+
 actualizarElem :: Int -> (a -> a) -> [a] -> [a]
-actualizarElem n f xs = error "COMPLETAR EJERCICIO 2"
+actualizarElem n f xs =  zipWith (actualizador f n) xs [0..]     -- armamos una lista de pares con (valor,indice)
+    where actualizador f n v i = if (i == n) then (f v) else v
 
 -- | infinito positivo (Haskell no tiene literal para +infinito)
 infinitoPositivo :: Float
